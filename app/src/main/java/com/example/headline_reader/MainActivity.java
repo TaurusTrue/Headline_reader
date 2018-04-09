946dc9a0452fd1e575d03d2bd3a6d6988dc413ab
“your_username” <67698619@qq.com>
2018-04-09 16:39:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/headline_reader/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.headline_reader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
